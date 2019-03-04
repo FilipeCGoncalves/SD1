b8df3edd712e8b45fd3df443c174675b64b99548
saar1312 <saar1312@yahoo.com>
2019-03-04 15:53:31 +0000

    Add: new service file that uses the logic of the TTT class game (just like the local one) but this time exposing the services through the API generated by the contract

---
+++ server/src/main/java/ttt/grpc/server/TTTServiceImpl.java
@@ -0,0 +1,10 @@
+package ttt.grpc.server;
+
+// Import the contract files
+
+// mvn will look for any path that matches
+// ttt/grpc/file_name.java 
+
+import ttt.grpc.TttContract;
+import ttt.grpc.TTTServiceGrpc;
+
